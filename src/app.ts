0e330aeb5a6b4f60114a8d723377baa040effdc4
mannarelli-killian <killian.mannarelli@outlook.fr>
2022-05-13 16:38:54 +0200

    fix : fixing docker-compose and port number

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ class App {
   constructor(routes: Routes[]) {
     this.app = express();
     this.env = NODE_ENV || 'development';
-    this.port = PORT || 300;
+    this.port = PORT || 3000;
 
     this.initializeMiddlewares();
     this.initializeRoutes(routes);
