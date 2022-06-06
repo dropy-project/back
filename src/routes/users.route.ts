8a8222e21d90790904288d3bb2e3db749c2f0cb0
Tomm Jobit <tjobit@MacBook-Air-de-Tomm.local>
2022-06-06 10:54:14 +0200

    fix : remove route get user

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import { Routes } from '@interfaces/routes.interface';
 import UsersController from '@controllers/users.controller';
-import authMiddleware from '@/middlewares/auth.middleware';
 
 class UsersRoute implements Routes {
   public path = '/user';
@@ -12,9 +11,7 @@ class UsersRoute implements Routes {
     this.initializeRoutes();
   }
 
-  private initializeRoutes() {
-    this.router.get(`${this.path}/:id`, authMiddleware, this.usersController.getUserById);
-  }
+  private initializeRoutes() { }
 }
 
 export default UsersRoute;
