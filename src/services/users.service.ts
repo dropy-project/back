610a0dbba3c47102066e2376bbe7185431d2335c
Killian Mannarelli <83612329+killian-mannarelli@users.noreply.github.com>
2022-07-11 11:56:36 +0200

    Time and distance required for background notifications (#69)
    * Feat : time and distance required for background notifications
    
    * Fix : Following PR comments
    
    * Fix : changing two lines

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -3,20 +3,29 @@ import { getAvailableDropiesAroundLocation } from '@/services/dropy.service';
 import { sendPushNotification } from '../notification';
 import client from '@/prisma/client';
 import { UserConversation } from '@/interfaces/chat.interface';
+import { getDistanceFromLatLonInMeters } from '@/utils/notification.utils';
+
+const DISTANCE_FILTER_RADIUS = 50;
+const TIME_FILTER_MINUTES = 60 * 24;
 
 export async function backgroundGeolocationPing(user: User, latitude: number, longitude: number, timeStamp: Date): Promise<Dropy[]> {
   const dropies = await getAvailableDropiesAroundLocation(latitude, longitude, user);
+  const sendNotification = await checkTimeAndDistanceBetweenNotifications(user, latitude, longitude, timeStamp);
   await client.user.update({
     where: {
       id: user.id,
     },
     data: {
-      lastSeenDate: timeStamp,
-      lastSeenLocationLatitude: latitude,
-      lastSeenLocationLongitude: longitude,
+      lastGeolocationPingDate: timeStamp,
+      lastGeolocationPingLatitude: latitude,
+      lastGeolocationPingLongitude: longitude,
     },
   });
 
+<<<<<<< HEAD
+  if (dropies.length > 0 && sendNotification) {
+    sendPushNotificationToUsers([user], 'Drop found near your position');
+=======
   if (dropies.length > 0) {
     sendPushNotification({
       user,
@@ -24,10 +33,21 @@ export async function backgroundGeolocationPing(user: User, latitude: number, lo
       body: 'Open the app to see it',
       sound: 'dropy_sound.mp3',
     });
+>>>>>>> main
   }
 
   return dropies;
 }
+
+export async function checkTimeAndDistanceBetweenNotifications(user: User, latitude: number, longitude: number, timeStamp: Date): Promise<Boolean> {
+  const { lastGeolocationPingDate, lastGeolocationPingLatitude, lastGeolocationPingLongitude } = user;
+  const distance = getDistanceFromLatLonInMeters(latitude, longitude, lastGeolocationPingLatitude, lastGeolocationPingLongitude);
+  const timeDifference = timeStamp.getTime() - lastGeolocationPingDate.getTime();
+  const timeDifferenceInMinutes = timeDifference / (1000 * 60);
+  return distance > DISTANCE_FILTER_RADIUS || timeDifferenceInMinutes > TIME_FILTER_MINUTES;
+}
+
+
 
 export async function updateDeviceToken(user: User, deviceToken: string): Promise<void> {
   await client.user.update({
