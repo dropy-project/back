80a10110209df96933f9a8b098c7a7ef59373d37
Killian Mannarelli <83612329+killian-mannarelli@users.noreply.github.com>
2022-07-18 17:09:22 +0200

    Debug pings notifs (#96)
    * Backgorund ping debug log
    
    * fix notif and console log
    
    * Add distance filter to dropies around array
    
    Co-authored-by: celian-rib <celian.riboulet@gmail.com>

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,4 +1,4 @@
-import { Dropy, User } from '@prisma/client';
+import { User } from '@prisma/client';
 import { getAvailableDropiesAroundLocation } from '@/services/dropy.service';
 import { sendPushNotification } from '../notification';
 import client from '@/prisma/client';
@@ -7,38 +7,56 @@ import { getDistanceFromLatLonInMeters } from '@/utils/notification.utils';
 const DISTANCE_FILTER_RADIUS = 50;
 const TIME_FILTER_MINUTES = 60 * 24;
 
-export async function backgroundGeolocationPing(user: User, latitude: number, longitude: number, timeStamp: Date): Promise<Dropy[]> {
-  const dropies = await getAvailableDropiesAroundLocation(latitude, longitude, user);
-  const sendNotification = await checkTimeAndDistanceBetweenNotifications(user, latitude, longitude, timeStamp);
-  await client.user.update({
-    where: {
-      id: user.id,
-    },
-    data: {
-      lastGeolocationPingDate: timeStamp,
-      lastGeolocationPingLatitude: latitude,
-      lastGeolocationPingLongitude: longitude,
-    },
+export async function backgroundGeolocationPing(user: User, latitude: number, longitude: number, timeStamp: Date): Promise<void> {
+  const dropiesAround = await getAvailableDropiesAroundLocation(latitude, longitude, user);
+
+  console.log();
+  console.log('-------------------');
+  console.log(`[BACKGROUND PING] - ${user.displayName}`);
+  console.log(`${latitude} , ${longitude}`);
+
+  const canSendNotification = await checkTimeAndDistanceBetweenNotifications(user, latitude, longitude, timeStamp);
+
+  const nearDropies = dropiesAround.filter(dropy => {
+    return getDistanceFromLatLonInMeters(latitude, longitude, dropy.latitude, dropy.longitude) <= DISTANCE_FILTER_RADIUS;
   });
 
-  if (dropies.length > 0 && sendNotification) {
-    sendPushNotification({
+  console.log(`Dropies around : ${dropiesAround.length}`);
+  console.log(`Dropies around : ${nearDropies.length}`);
+  console.log(`Send notification : ${canSendNotification}`);
+  console.log('-------------------');
+
+  if (nearDropies.length > 0 && canSendNotification) {
+    console.log('Send notification and save into the database');
+    await sendPushNotification({
       user,
       title: 'Drop found near your position!',
       body: 'Open the app to see it',
       sound: 'dropy_sound.mp3',
     });
-  }
 
-  return dropies;
+    await client.user.update({
+      where: { id: user.id },
+      data: {
+        lastGeolocationPingDate: timeStamp,
+        lastGeolocationPingLatitude: latitude,
+        lastGeolocationPingLongitude: longitude,
+      },
+    });
+  }
 }
 
 export async function checkTimeAndDistanceBetweenNotifications(user: User, latitude: number, longitude: number, timeStamp: Date): Promise<Boolean> {
   const { lastGeolocationPingDate, lastGeolocationPingLatitude, lastGeolocationPingLongitude } = user;
   if (lastGeolocationPingDate == null || lastGeolocationPingLatitude == null || lastGeolocationPingLongitude == null) return true;
+
   const distance = getDistanceFromLatLonInMeters(latitude, longitude, lastGeolocationPingLatitude, lastGeolocationPingLongitude);
+  console.log(`Distance from last ping : ${distance}m`);
+
   const timeDifference = timeStamp.getTime() - lastGeolocationPingDate.getTime();
   const timeDifferenceInMinutes = timeDifference / (1000 * 60);
+  console.log(`Time from last ping : ${timeDifferenceInMinutes}m`);
+
   return distance > DISTANCE_FILTER_RADIUS || timeDifferenceInMinutes > TIME_FILTER_MINUTES;
 }
 
