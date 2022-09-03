c54b2f62678991ed1036523ab4a8fb1fd83bde4a
Célian Riboulet <celian.riboulet@gmail.com>
2022-09-03 21:34:40 +0200

    Museum route (#126)

--- src/interfaces/dropy.interface.ts
+++ src/interfaces/dropy.interface.ts
@@ -1,17 +1,12 @@
 import { Dropy } from '@prisma/client';
+import { SimplifiedUser } from './user.interface';
 
 export type DropyAround = Pick<Dropy, 'id' | 'creationDate' | 'latitude' | 'longitude'>;
 
 export type SimplifiedDropy = Pick<Dropy, 'id' | 'mediaUrl' | 'mediaType' | 'latitude' | 'longitude' | 'creationDate' | 'retrieveDate'>;
 
 export type DropyWithUsers = SimplifiedDropy & {
-  emitterId: number;
-  emitterDisplayName: string;
-  emitterAvatarUrl: string;
-
-  retrieverId?: number;
-  retrieverDisplayName?: string;
-  retrieverAvatarUrl?: string;
-
+  emitter: SimplifiedUser;
+  retriever?: SimplifiedUser;
   conversationId: number;
 };
