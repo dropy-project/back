55af2f0f38e5e782023a5404f6c9709e936224bf
Guillaume <guillaume.froidcourt@etu.u-bordeaux.fr>
2022-09-18 16:36:55 +0200

    fix max energy to 90 and min to 0

--- src/services/socket/services/user.socket.service.ts
+++ src/services/socket/services/user.socket.service.ts
@@ -2,6 +2,13 @@ import client from '@/client';
 import { User } from '@prisma/client';
 
 export async function incrementUserEnergy(user: User, energyModification: number): Promise<void> {
+  if (user.energy + energyModification >= 90) {
+    user.energy = 90;
+  } else if (user.energy + energyModification <= 0) {
+    user.energy = 0;
+  } else {
+    user.energy += energyModification;
+  }
   await client.user.update({
     where: { id: user.id },
     data: {
