a97bd32a797e28233e676640370468276f5fcacc
mannarelli-killian <killian.mannarelli@outlook.fr>
2022-11-26 23:16:01 +0100

    fix : Fix mistakes

--- src/middlewares/doublesocket.middleware.ts
+++ src/middlewares/doublesocket.middleware.ts
@@ -3,14 +3,16 @@ import { Namespace } from 'socket.io';
 import { getUsersSockets } from '@/utils/socket.utils';
 import { AuthenticatedSocket } from '@/interfaces/auth.interface';
 
-const doubleSocketMiddleware = async (socket: AuthenticatedSocket, namespace: Namespace) => {
+const doubleSocketMiddleware = async (socket: AuthenticatedSocket, namespace: Namespace, next : NextFunction) => {
   try {
     const sockets = await getUsersSockets(namespace, [socket.user]);
     if (sockets.length > 1) {
       socket.disconnect();
     }
+    next();
   } catch (error) {
     console.log('ERROR : Somebody is connecting using two devices');
+    next();
   }
 };
 
